fc4152cb56bd0180e43ceedab273afacbd39598a
krutik09 <skrutik9@gmail.com>
2022-09-07 14:03:01 +0530

    Updates

---
+++ precache-manifest.047772bc0c6860c09a632a59cce4107a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce212f3be36c03842c62d202e6d981de",
+    "url": "/krutik09-20dcs112/index.html"
+  },
+  {
+    "revision": "44f65ac15abf3e84ef50",
+    "url": "/krutik09-20dcs112/static/css/2.9229e6f1.chunk.css"
+  },
+  {
+    "revision": "9684f9301a28435b836e",
+    "url": "/krutik09-20dcs112/static/css/main.48551862.chunk.css"
+  },
+  {
+    "revision": "44f65ac15abf3e84ef50",
+    "url": "/krutik09-20dcs112/static/js/2.29c5b276.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/krutik09-20dcs112/static/js/2.29c5b276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9684f9301a28435b836e",
+    "url": "/krutik09-20dcs112/static/js/main.dd3bfe68.chunk.js"
+  },
+  {
+    "revision": "c9e3d9bf44473cb20269",
+    "url": "/krutik09-20dcs112/static/js/runtime-main.a1549500.js"
+  }
+]);
